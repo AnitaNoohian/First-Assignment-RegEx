0514641d7fb6d0874cdb5e6eded6f3453aa77607
AnitaNoohian <noohian83@gmail.com>
2024-02-27 22:49:36 +0330

    First Code

--- src/main/java/Exercise3.java
+++ src/main/java/Exercise3.java
@@ -11,7 +11,7 @@ public class Exercise3 {
     */
 
     public static String extractURL(String text) {
-        String regex = "write your regex pattern here!";  // TODO
+        String regex = "http.*://.+";  // TODO
 
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(text);
